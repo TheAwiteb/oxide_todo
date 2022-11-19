32ef9caa2cabb84624fdb89e8b63c988f9726605
TheAwiteb <awiteb@hotmail.com>
2022-11-19 11:36:40 +0300

    Enable ratelimiting

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,3 +1,4 @@
+use actix_governor::Governor;
 use actix_web::web;
 
 pub mod login;
@@ -7,10 +8,23 @@ pub mod traits;
 pub mod utils;
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
+    let bearer_limit_config = crate::ratelimit::init_bearer();
+    let ip_limit_config = crate::ratelimit::init_ip();
+
     cfg.service(
         web::scope("/auth")
-            .service(register::register)
-            .service(login::login)
-            .service(revoke::revoke),
+            .service(
+                // Endpoints that dosn't require authentication
+                web::scope("")
+                    .wrap(Governor::new(&ip_limit_config))
+                    .service(register::register)
+                    .service(login::login),
+            )
+            .service(
+                // Endpoints that require authentication
+                web::scope("")
+                    .wrap(Governor::new(&bearer_limit_config))
+                    .service(revoke::revoke),
+            ),
     );
 }
