84a52743d294f4f37116162ba63ad716050b41ed
TheAwiteb <awiteb@hotmail.com>
2022-11-20 14:48:29 +0300

    Remove ratelimiter from `auth::init_routes`

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,4 +1,3 @@
-use actix_governor::Governor;
 use actix_web::web;
 
 pub mod login;
@@ -8,23 +7,10 @@ pub mod traits;
 pub mod utils;
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
-    let bearer_limit_config = crate::ratelimit::init_bearer();
-    let ip_limit_config = crate::ratelimit::init_ip();
-
     cfg.service(
         web::scope("/auth")
-            .service(
-                // Endpoints that dosn't require authentication
-                web::scope("")
-                    .wrap(Governor::new(&ip_limit_config))
-                    .service(register::register)
-                    .service(login::login),
-            )
-            .service(
-                // Endpoints that require authentication
-                web::scope("")
-                    .wrap(Governor::new(&bearer_limit_config))
-                    .service(revoke::revoke),
-            ),
+            .service(register::register)
+            .service(login::login)
+            .service(revoke::revoke),
     );
 }
