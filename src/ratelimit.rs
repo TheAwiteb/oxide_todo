afe9d8d951672f05184aecca96ded0359ac63494
TheAwiteb <awiteb@hotmail.com>
2022-11-25 23:22:57 +0300

    Replace `actix-governor` with `actix_extensible_rate_limit`

--- src/ratelimit.rs
+++ src/ratelimit.rs
@@ -1,59 +1,46 @@
-use std::env;
+use std::{env, future::Ready, time::Duration};
 
-use actix_governor::{GovernorConfig, GovernorConfigBuilder};
-use actix_web::{dev::ServiceRequest, HttpResponse, HttpResponseBuilder};
-use governor::{
-    clock::{Clock, DefaultClock, QuantaInstant},
-    middleware::StateInformationMiddleware,
-    NotUntil,
+use actix_extensible_rate_limit::{
+    backend::{memory::InMemoryBackend, SimpleInput, SimpleInputFunctionBuilder, SimpleOutput},
+    HeaderCompatibleOutput, RateLimiter, RateLimiterBuilder,
 };
+use actix_web::{dev::ServiceRequest, http::StatusCode, HttpResponse};
 
-use crate::errors::TodoError as TodoErrorTrait;
 use crate::{errors::Error as TodoError, schemas::errors::ErrorSchema};
 
-#[derive(Debug, Clone)]
-pub struct IpAddressExtractor;
+/// The response error for rate limit exceeded
+fn rate_limit_exceeded(rate_info: &SimpleOutput) -> HttpResponse {
+    let rest_time = rate_info.seconds_until_reset();
+    let body = ErrorSchema::from(TodoError::TooManyRequests(rest_time));
 
-impl actix_governor::KeyExtractor for IpAddressExtractor {
-    type Key = String;
-    type KeyExtractionError = TodoError;
-
-    fn extract(&self, req: &ServiceRequest) -> Result<Self::Key, Self::KeyExtractionError> {
-        req.connection_info()
-            .peer_addr()
-            .map(Into::into)
-            .server_err("Could not get IP address")
-    }
-
-    fn exceed_rate_limit_response(
-        &self,
-        negative: &NotUntil<QuantaInstant>,
-        mut res: HttpResponseBuilder,
-    ) -> HttpResponse {
-        let wait_time = negative
-            .wait_time_from(DefaultClock::default().now())
-            .as_secs();
-        res.json(ErrorSchema::from(TodoError::TooManyRequests(wait_time)))
-    }
+    HttpResponse::build(StatusCode::TOO_MANY_REQUESTS)
+        .append_header(("x-ratelimit-limit", rate_info.limit()))
+        .append_header(("x-ratelimit-remaining", rate_info.remaining()))
+        .append_header(("x-ratelimit-reset", rest_time))
+        .json(body)
 }
 
 /// Initializes IP rate limiter
-pub fn init_ip() -> GovernorConfig<IpAddressExtractor, StateInformationMiddleware> {
-    GovernorConfigBuilder::default()
-        .key_extractor(IpAddressExtractor)
-        .per_second(
-            env::var("RATE_LIMIT_PER_SECOND")
-                .unwrap_or_else(|_| "5".to_string())
-                .parse()
-                .expect("Invalid rate limit per second"),
-        )
-        .burst_size(
-            env::var("RATE_LIMIT_BURST_SIZE")
-                .unwrap_or_else(|_| "60".to_string())
-                .parse()
-                .expect("Invalid rate limit burst size"),
-        )
-        .use_headers()
-        .finish()
-        .unwrap()
+pub fn init_ip(
+    backend: InMemoryBackend,
+) -> RateLimiterBuilder<
+    InMemoryBackend,
+    SimpleOutput,
+    impl Fn(&ServiceRequest) -> Ready<Result<SimpleInput, actix_web::Error>>,
+> {
+    let seconds: u64 = env::var("RATE_LIMIT_PER_SECOND")
+        .unwrap_or_else(|_| "60".to_string())
+        .parse()
+        .expect("Invalid rate limit per second");
+    let burst_size: u64 = env::var("RATE_LIMIT_BURST_SIZE")
+        .unwrap_or_else(|_| "30".to_string())
+        .parse()
+        .expect("Invalid rate limit burst size");
+    // Assign a limit of `burst_size` requests per `seconds` seconds per client ip address
+    let input = SimpleInputFunctionBuilder::new(Duration::from_secs(seconds), burst_size)
+        .real_ip_key()
+        .build();
+    RateLimiter::builder(backend, input)
+        .add_headers()
+        .request_denied_response(rate_limit_exceeded)
 }
