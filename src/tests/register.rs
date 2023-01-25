3177f3d4db6451417791e15777618ec3a2da5268
TheAwiteb <awiteb@hotmail.com>
2023-01-26 02:02:02 +0300

    Uncomment the todo, after fix #7

--- src/tests/register.rs
+++ src/tests/register.rs
@@ -1,5 +1,4 @@
 use actix_web::{web, App};
-use serial_test::serial;
 
 use super::init_test_pool;
 use crate::{auth, schemas::auth::RegisterSchema};
@@ -18,12 +17,10 @@ async fn register_request(username: String, password: String) -> super::TestResp
 #[rstest::rstest]
 #[case::register_with_correct_credentials("testusername1", "testpassword", 201)]
 #[case::register_with_already_existing_username("testusername1", "testpassword", 400)]
-// TODO: uncomment  after solve this issue https://github.com/TheAwiteb/restful_todo/issues/7
-// #[case::empty_username("", "testpassword", 400)]
-// #[case::empty_password("testusername1", "", 400)]
-// #[case::empty_username_and_password("", "", 400)]
+#[case::empty_username("", "testpassword", 400)]
+#[case::empty_password("testusername1", "", 400)]
+#[case::empty_username_and_password("", "", 400)]
 #[actix_web::test]
-#[serial]
 async fn register_endpoint(
     #[case] username: String,
     #[case] password: String,
