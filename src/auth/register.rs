8916a4c86d7fb7f331d4c7ca46a7045689dc1187
TheAwiteb <awiteb@hotmail.com>
2023-01-26 00:59:49 +0300

    Make `` expected `StatusCode` instead of `u16`

--- src/auth/register.rs
+++ src/auth/register.rs
@@ -1,4 +1,4 @@
-use actix_web::{post, web, Responder};
+use actix_web::{http::StatusCode, post, web, Responder};
 use sea_orm::DatabaseConnection;
 
 use crate::schemas::{auth::RegisterSchema, errors::ErrorSchema};
@@ -27,9 +27,8 @@ pub async fn register(
     payload: web::Json<RegisterSchema>,
 ) -> impl Responder {
     log::info!("Registering user: {}", payload.username);
-    // 201 Created response code
     payload
         .create(db.get_ref())
         .await
-        .map(|user| user.with_code(201))
+        .map(|user| user.with_code(StatusCode::CREATED))
 }
