72955f84085b35995c86df60e706f82033a827e3
TheAwiteb <awiteb@hotmail.com>
2023-02-12 03:56:41 +0300

    Moving `src/tests/create_todo.rs` -> `src/tests/todo/create_todo.rs`

--- src/tests/todo/create_todo.rs
+++ src/tests/todo/create_todo.rs
@@ -1,16 +1,12 @@
 use actix_web::{web, App};
 use serde_json::json;
 
-use super::init_test_pool;
-use super::login::login_req;
 use crate::schemas::user::UserSchema;
+use crate::tests::login::login_req;
+use crate::tests::{check_content_length, check_content_type, init_test_pool, TestResponseType};
 use crate::todo;
 
-pub async fn create_todo_req(
-    title: String,
-    status: String,
-    token: String,
-) -> super::TestResponseType {
+pub async fn create_todo_req(title: String, status: String, token: String) -> TestResponseType {
     let pool = init_test_pool().await;
     let srv = actix_test::start(move || {
         App::new()
@@ -18,7 +14,7 @@ pub async fn create_todo_req(
             .service(todo::create::create)
     });
     srv.post("/")
-        .insert_header(("Authorization", token))
+        .insert_header(("Authorization", format!("Bearer {token}")))
         .send_json(&json!({"title": title, "status": status}))
         .await
         .unwrap()
@@ -38,7 +34,7 @@ async fn create_todo_endpoint(
     #[case] status_code: u16,
 ) {
     let user: UserSchema = serde_json::from_slice(
-        login_req("Awiteb".to_owned(), "123456".to_owned())
+        login_req("testusername1".to_owned(), "testpassword".to_owned())
             .await
             .body()
             .await
@@ -50,6 +46,6 @@ async fn create_todo_endpoint(
 
     let create_todo_res = create_todo_req(title, status, user.token).await;
     assert_eq!(create_todo_res.status(), status_code);
-    super::check_content_type(&create_todo_res);
-    super::check_content_length(&create_todo_res);
+    check_content_type(&create_todo_res);
+    check_content_length(&create_todo_res);
 }
