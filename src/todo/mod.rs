07e7505d9e7e80f146e894744c9185a6138c20a7
TheAwiteb <awiteb@hotmail.com>
2023-02-13 10:23:47 +0300

    Add todo list endpoint

--- src/todo/mod.rs
+++ src/todo/mod.rs
@@ -1,8 +1,14 @@
 use actix_web::web;
 
 pub mod create;
+pub mod list;
+pub mod queries;
 pub mod utils;
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/todo").service(create::create));
+    cfg.service(
+        web::scope("/todo")
+            .service(create::create)
+            .service(list::list),
+    );
 }
