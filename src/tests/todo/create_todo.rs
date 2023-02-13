918951686fca98a7de18ad81014c673119b38279
TheAwiteb <awiteb@hotmail.com>
2023-02-13 18:18:06 +0300

    Update create todo tests

--- src/tests/todo/create_todo.rs
+++ src/tests/todo/create_todo.rs
@@ -4,17 +4,26 @@ use serde_json::json;
 use crate::schemas::user::UserSchema;
 use crate::tests::login::login_req;
 use crate::tests::{check_content_length, check_content_type, init_test_pool, TestResponseType};
-use crate::todo;
 
-pub async fn create_todo_req(title: String, status: String, token: String) -> TestResponseType {
+pub async fn create_todo_req(title: String, status: String) -> TestResponseType {
     let pool = init_test_pool().await;
+    let user: UserSchema = serde_json::from_slice(
+        login_req("testusername1".to_owned(), "testpassword".to_owned())
+            .await
+            .body()
+            .await
+            .unwrap()
+            .to_vec()
+            .as_slice(),
+    )
+    .unwrap();
     let srv = actix_test::start(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
-            .service(todo::create::create)
+            .service(web::scope("/todo").service(crate::todo::create::create))
     });
-    srv.post("/")
-        .insert_header(("Authorization", format!("Bearer {token}")))
+    srv.post("/todo")
+        .insert_header(("Authorization", format!("Bearer {}", user.token)))
         .send_json(&json!({"title": title, "status": status}))
         .await
         .unwrap()
@@ -22,6 +31,10 @@ pub async fn create_todo_req(title: String, status: String, token: String) -> Te
 
 #[rstest::rstest]
 #[case::create_todo_with_correct_credentials("testtitle1", "completed", 200)]
+#[case::create_todo_with_correct_credentials("testtitle2", "pending", 200)]
+#[case::create_todo_with_correct_credentials("testtitle3", "progress", 200)]
+#[case::create_todo_with_correct_credentials("testtitle4", "cancelled", 200)]
+#[case::create_todo_with_existing_title("testtitle1", "completed", 400)]
 #[case::create_todo_with_empty_title("", "pending", 400)]
 #[case::create_todo_with_empty_status("testtitle2", "", 400)]
 #[case::create_todo_with_empty_title_and_status("", "", 400)]
@@ -33,19 +46,12 @@ async fn create_todo_endpoint(
     #[case] status: String,
     #[case] status_code: u16,
 ) {
-    let user: UserSchema = serde_json::from_slice(
-        login_req("testusername1".to_owned(), "testpassword".to_owned())
-            .await
-            .body()
-            .await
-            .unwrap()
-            .to_vec()
-            .as_slice(),
-    )
-    .unwrap();
-
-    let create_todo_res = create_todo_req(title, status, user.token).await;
+    let create_todo_res = create_todo_req(title, status).await;
     assert_eq!(create_todo_res.status(), status_code);
     check_content_type(&create_todo_res);
     check_content_length(&create_todo_res);
+    // if the status code is 200, wait for 1 second to make a time lag to be tested in list todo endpoint
+    if status_code == 200 {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
